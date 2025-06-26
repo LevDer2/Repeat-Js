31773477d533cdad0fdda72665d944396efc051d
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-06-26 14:39:51 +0300

    start

--- Day 1/js/js.js
+++ Day 1/js/js.js
@@ -1,1 +1,14 @@
+// let name = "Lev"
+// console.log(name);
+// name = 12;
+// console.log(name);
+var a;
+var b = 10;
+console.log(`a = ${a}`);
+console.log(`b = ${b}`);
 
+a = 7
+b = a
+
+console.log(`a = ${a}`);
+console.log(`b = ${b}`);
